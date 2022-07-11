7472d20359381b1ca29c5b7651282b27dabae992
feg59crz <fernandogomescruz@gmail.com>
2022-07-11 19:06:41 -0300

    virt-manager and docker

--- configuration.nix
+++ configuration.nix
@@ -1,9 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
+let
 
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -18,11 +16,6 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -86,7 +79,7 @@
   users.users.eee = {
     isNormalUser = true;
     description = "eee";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "docker" "wheel" "libvirtd" ];
     packages = with pkgs; [
       firefox
     #  thunderbird
@@ -99,8 +92,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    virt-manager
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -113,21 +105,16 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # OpenSSH daemon.
+  services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Docker
+  virtualisation.docker.enable = true;
+  
+  # Libvirtd
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+  
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
-
 }
